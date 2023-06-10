0308368180e376d2d6243ce1401b741794aa2d4c
Melanie Richards <mr@melanie-richards.com>
2023-06-09 17:44:29 -0700

    Restructure resources by category

--- src/_data/categories.js
+++ src/_data/categories.js
@@ -8,14 +8,14 @@ module.exports = async () => {
     const response = await notion.databases.query({
       database_id: databaseId,
       filter: {
-        property: "Tags",
-        multi_select: {
-          does_not_contain: "Private"
+        property: "Private",
+        checkbox: {
+          equals: false
         }
       },
       sorts: [
         {
-          property: "Tags",
+          property: "Category",
           direction: "ascending"
         },
         {
@@ -30,8 +30,23 @@ module.exports = async () => {
     });
 
     const resources = response.results.map(resource => resource.properties);
-    console.log(resources);
-    return resources;
+
+    // Transform the response to restructure resources by category
+    const categories = Object.values(resources.reduce((accumulator, current_value) => {
+      let catName = current_value.Category.select.name;
+
+      //  If the category attached to the resource is not already in categories, add it
+      if (!accumulator.hasOwnProperty(catName)) {
+        accumulator[catName] = {name: catName, items: []}
+      }
+
+      // Add the resource itself to the category
+      accumulator[catName].items.push(current_value);
+
+      return accumulator;
+    }, {}));
+    console.log(categories);
+    return categories;
   } catch(error) {
     console.error(error.body);
   }
