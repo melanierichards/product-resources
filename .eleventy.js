396debcb212eed50a716dac917ab79b34a5a2cab
Melanie Richards <mr@melanie-richards.com>
2023-06-09 18:10:33 -0700

    Add style dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function(eleventyConfig) {
 
+  // Don't try to build asset files, just transparently copy them through
+  eleventyConfig.addPassthroughCopy('src/assets');
+
   // Customize input directory
   return {
     dir: {
