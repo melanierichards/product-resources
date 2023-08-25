636bedb95a967156b2508606d5e2217b56b27431
Melanie Richards <mr@melanie-richards.com>
2023-08-24 21:25:27 -0700

    Disallow GPTBot, OpenAI's web crawler

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 module.exports = function(eleventyConfig) {
 
-  // Don't try to build asset files, just transparently copy them through
+  // Don't try to build pages from these files
   eleventyConfig.addPassthroughCopy('src/assets');
+  eleventyConfig.addPassthroughCopy('README.md');
+  eleventyConfig.addPassthroughCopy('src/robots.txt');
 
   // Customize input directory
   return {
